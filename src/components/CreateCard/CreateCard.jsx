235b300e5bce9db383b8e1fc20324c52d2306836
Mark Klein <kleincentral@gmail.com>
2024-01-29 15:11:31 -0600

    Checkboxes now work for the categories rendering all of them from a dummy list

--- src/components/CreateCard/CreateCard.jsx
+++ src/components/CreateCard/CreateCard.jsx
@@ -8,41 +8,77 @@ export default function CreateCard() {
   let [UPCNumber, setUPCNumber] = useState("");
   let [vendorStyle, setVendorStyle] = useState("");
 
+  const dummyCategories = [
+    { id: 1, name: "goats" },
+    { id: 2, name: "pikefish" },
+    { id: 3, name: "stuck in placehold data" },
+  ];
+
+  const getCategories = () => {
+    let checkboxes = document.querySelectorAll(
+      'input[name="categories"]:checked'
+    );
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+      values.push(checkbox.value);
+    });
+    console.log(values);
+    return values;
+  };
+
   // User hits submit button, POSTs a new card, redirects back to /cards
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     console.log("In handleSubmit");
+    e.preventDefault();
     // Do the dispatch
-    history.push("/cards");
+    // history.push("/cards");
+    console.log(variationName, UPCNumber, vendorStyle, getCategories());
   };
 
   return (
     <div className="container">
       <form>
+        Variation Name
         <input
           value={variationName}
           label="Variation Name"
           placeholder="Variation Name"
           onChange={() => setVariationName(event.target.value)}
+          id="variation"
         />
+        <br></br>
+        UPC Number
         <input
           value={UPCNumber}
           label="UPC Number"
           placeholder="UPC Number"
           onChange={() => setUPCNumber(event.target.value)}
+          id="UPCNumber"
         />
+        <br></br>
+        Vendor Style
         <input
           value={vendorStyle}
           label="Vendor Style"
           placeholder="Vendor Style"
           onChange={() => setVendorStyle(event.target.value)}
+          id="vendorStyle"
         />
-        <p>Select your favorite colors:</p>
-        <input type="checkbox" name="color" value="red" id="c1" />
-        <input type="checkbox" name="color" value="green" id="c2" />
-        <input type="checkbox" name="color" value="blue" id="c3" />
-        <p>
-          <button id="btn">Get Selected Colors</button>
-        </p>
+        <p>Categories</p>
+        {dummyCategories[0] &&
+          dummyCategories.map((index) => {
+            return (
+              <div>
+                {index.name}
+                <input
+                  type="checkbox"
+                  name="categories"
+                  value={index.name}
+                  id={index.id}
+                />
+              </div>
+            );
+          })}
         Create Card
         <button onClick={handleSubmit}>Submit</button>
       </form>
