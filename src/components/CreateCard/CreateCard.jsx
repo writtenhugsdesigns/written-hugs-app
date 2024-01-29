a90f172f6de0ca174e1125a819f5c863ba59eb66
Mark Klein <kleincentral@gmail.com>
2024-01-29 11:14:18 -0600

    Database work finished

--- src/components/CreateCard/CreateCard.jsx
+++ src/components/CreateCard/CreateCard.jsx
@@ -1,19 +1,19 @@
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
 
-export default function CreateCard(){
-    const history = useHistory();
+export default function CreateCard() {
+  const history = useHistory();
 
-    // User hits submit button, POSTs a new card, redirects back to /cards
-    const handleSubmit = () => {
-        console.log("In handleSubmit");
-        // Do the dispatch
-        history.push("/cards");
-    }
+  // User hits submit button, POSTs a new card, redirects back to /cards
+  const handleSubmit = () => {
+    console.log("In handleSubmit");
+    // Do the dispatch
+    history.push("/cards");
+  };
 
-    return (
-        <div className = 'container'>
-            Create Card
-            <button onClick = {handleSubmit}>Submit</button>
-        </div>
-    )
+  return (
+    <div className="container">
+      Create Card
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
