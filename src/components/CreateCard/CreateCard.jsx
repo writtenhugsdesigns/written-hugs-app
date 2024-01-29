b0e61a560b646277c90d4e9fc58107b78d857568
Mark Klein <kleincentral@gmail.com>
2024-01-29 11:40:04 -0600

    Form started, added 3 input fields

--- src/components/CreateCard/CreateCard.jsx
+++ src/components/CreateCard/CreateCard.jsx
@@ -1,19 +1,44 @@
-import { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
-export default function CreateCard(){
-    const history = useHistory();
+export default function CreateCard() {
+  const history = useHistory();
 
-    // User hits submit button, POSTs a new card, redirects back to /cards
-    const handleSubmit = () => {
-        console.log("In handleSubmit");
-        // Do the dispatch
-        history.push("/cards");
-    }
+  let [variationName, setVariationName] = useState("");
+  let [UPCNumber, setUPCNumber] = useState("");
+  let [vendorStyle, setVendorStyle] = useState("");
 
-    return (
-        <div className = 'container'>
-            Create Card
-            <button onClick = {handleSubmit}>Submit</button>
-        </div>
-    )
+  // User hits submit button, POSTs a new card, redirects back to /cards
+  const handleSubmit = () => {
+    console.log("In handleSubmit");
+    // Do the dispatch
+    history.push("/cards");
+  };
+
+  return (
+    <div className="container">
+      <form>
+        <input
+          value={variationName}
+          label="Variation Name"
+          placeholder="Variation Name"
+          onChange={() => setVariationName(event.target.value)}
+        />
+        <input
+          value={UPCNumber}
+          label="UPC Number"
+          placeholder="UPC Number"
+          onChange={() => setUPCNumber(event.target.value)}
+        />
+        <input
+          value={vendorStyle}
+          label="Vendor Style"
+          placeholder="Vendor Style"
+          onChange={() => setVendorStyle(event.target.value)}
+        />
+        Create Card
+        <button onClick={handleSubmit}>Submit</button>
+      </form>
+    </div>
+  );
 }
