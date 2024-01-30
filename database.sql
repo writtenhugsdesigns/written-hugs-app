bcd4370868a66806e0a5da88f1d70e5541eafac9
Mark Klein <kleincentral@gmail.com>
2024-01-30 14:47:16 -0600

    Final database changes to appease the great sql god james

--- database.sql
+++ database.sql
@@ -9,15 +9,6 @@ create table "user" (
     "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
-create table "stickers" (
-	"id" serial primary key,
-	"name" varchar,
-	"sticker_jpeg" varchar,
-	"sticker_pdf" varchar,
-    "inserted_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-    "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now()
-);
-
 create table "cards" (
 	"id" serial primary key,
 	"name" varchar,
@@ -32,7 +23,8 @@ create table "cards" (
 	"insert_img" varchar,
 	"insert_ai" varchar,
 	"raw_art" varchar,
-	"sticker_id" integer references "stickers",
+	"sticker_jpeg" varchar,
+    "sticker_pdf" varchar,
     "inserted_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
     "updated_at" TIMESTAMPTZ NOT NULL DEFAULT now()
 );
@@ -98,11 +90,6 @@ BEFORE UPDATE ON "user"
 FOR EACH ROW
 EXECUTE PROCEDURE set_updated_at_to_now();
 
-CREATE TRIGGER on_stickers_update
-BEFORE UPDATE ON "stickers"
-FOR EACH ROW
-EXECUTE PROCEDURE set_updated_at_to_now();
-
 CREATE TRIGGER on_cards_update
 BEFORE UPDATE ON "cards"
 FOR EACH ROW
@@ -140,15 +127,10 @@ INSERT INTO "user"
 VALUES
 ('Test1', 'password', 'ADMIN'); -- this password is not hashed, so don't attempt to log in like this
 
-INSERT INTO "stickers"
-("name", "sticker_jpeg", "sticker_pdf")
-VALUES
-('goat', 'https://drive.google.com/file/d/1Oq0VCUGznClS7ohgu2r_xP-4BuqygzHn/view?usp=sharing', 'https://drive.google.com/file/d/1tJ3DhRV25aUASp_SwC5b2HfnxJAIwQT4/view?usp=sharing');
-
 INSERT INTO "cards"
-("name", "category", "description", "UPC", "SKU", "barcode", "front_img", "front_tiff", "inner_img", "insert_img", "insert_ai", "raw_art", "sticker_id")
+("name", "category", "description", "upc", "sku", "barcode", "front_img", "front_tiff", "inner_img", "insert_img", "insert_ai", "raw_art", "sticker_jpeg", "sticker_pdf")
 VALUES
-('more goat', 'goats', 'dam thats a lot of goats', 111, 1111, 11111, 'https://drive.google.com/file/d/1w82PH57o43XCZt87nxZS3OHikUzkfkBV/view?usp=sharing', 'https://drive.google.com/file/d/1dwMYyhnjNdhXUu30PmmHKN1be243VDlZ/view?usp=sharing', 'inner image?', 'insert image?', 'https://drive.google.com/file/d/1OgZMW-bqU2igOazlL6vhQ6r1qOBrysQ4/view?usp=sharing', 'RAW ART GOES HERE', 1);
+('more goat', 'goats', 'dam thats a lot of goats', 111, 1111, 11111, 'https://drive.google.com/file/d/1w82PH57o43XCZt87nxZS3OHikUzkfkBV/view?usp=sharing', 'https://drive.google.com/file/d/1dwMYyhnjNdhXUu30PmmHKN1be243VDlZ/view?usp=sharing', 'inner image?', 'insert image?', 'https://drive.google.com/file/d/1OgZMW-bqU2igOazlL6vhQ6r1qOBrysQ4/view?usp=sharing', 'RAW ART GOES HERE', 'https://drive.google.com/file/d/1Oq0VCUGznClS7ohgu2r_xP-4BuqygzHn/view?usp=sharing', 'https://drive.google.com/file/d/1tJ3DhRV25aUASp_SwC5b2HfnxJAIwQT4/view?usp=sharing');
 
 INSERT INTO "wholesalers"
 ("company_name", "user_id")
