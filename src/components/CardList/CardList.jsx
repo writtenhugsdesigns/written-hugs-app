aa47a7edbbc8e865109aa5d20df603978a04c67e
Mark Klein <kleincentral@gmail.com>
2024-01-30 10:24:06 -0600

    Card List and Card Page populated with some dummy data to take its information

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,7 +1,26 @@
-export default function CardList(){
-    return (
-        <div className = 'container'>
-            Card List
-        </div>
-    )
+export default function CardList({ index }) {
+  const viewIndex = () => {
+    console.log("This will do pop up stuff Ig.");
+  };
+
+  const editIndex = () => {
+    console.log("This will do pop up stuff for edit.");
+  };
+
+  const deleteIndex = () => {
+    console.log("BEGONE THINGY WITH INDEX", index.id);
+  };
+
+  return (
+    <tr>
+      <td>{index.category}</td>
+      <td>{index.inserted_at}</td>
+      <td>{index.description}</td>
+      <td>
+        <button onClick={viewIndex}>View</button>
+        <button onClick={editIndex}>Edit</button>
+        <button onClick={deleteIndex}>Delete</button>
+      </td>
+    </tr>
+  );
 }
