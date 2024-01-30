a3793825052259d4a8c3cd6568ee47c380b8baea
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-01-30 09:37:11 -0600

    Styled links on admin home page

--- src/components/AdminHome/AdminHome.jsx
+++ src/components/AdminHome/AdminHome.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import './AdminHome.css';
 
 function AdminHome() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
+  // const user = useSelector((store) => store.user);
   const history = useHistory();
 
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-
-      <button onClick = {() => history.push("/cards")}>Manage Cards</button>
-      <button onClick = {() => history.push("/wholesalers")}>Manage Wholesalers</button>
-      <button onClick = {() => history.push("/pitches")}>Manage Pitches</button>
-      
+      <div className = 'adminMenu'>
+        <button className = 'adminMenuItem' onClick = {() => history.push("/cards")}>Manage Cards</button>
+        <button className = 'adminMenuItem' onClick = {() => history.push("/wholesalers")}>Manage Wholesalers</button>
+        <button className = 'adminMenuItem' onClick = {() => history.push("/pitches")}>Manage Pitches</button>
+      </div>
     </div>
   );
 }
 
-// this allows us to use <App /> in index.js
+
 export default AdminHome;
