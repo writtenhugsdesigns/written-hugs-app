a65a98b96906f71ba87d3b207ac58ea9244e1f1a
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-01 15:44:14 -0600

    create card formatting

--- src/components/CreateCard/CreateCard.jsx
+++ src/components/CreateCard/CreateCard.jsx
@@ -1,6 +1,9 @@
 import { useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { ArrowBackIos } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import './CreateCard.css';
 
 export default function CreateCard() {
   const history = useHistory();
@@ -70,118 +73,164 @@ export default function CreateCard() {
         getCategories()
       );
     }
+    // Clear fields
+    setVariationName(null);
+    setUPCNumber(null);
+    setVendorStyle(null);
   };
+
+  // Clears form inputs when user hits the clear button
+  const handleClear = (e) => {
+    e.preventDefault();
+    setVariationName(null);
+    setUPCNumber(null);
+    setVendorStyle(null);
+  }
 
   return (
     <div className="container">
-      <form>
-        Variation Name
-        <input
-          value={variationName}
-          label="Variation Name"
-          placeholder="Variation Name"
-          onChange={() => setVariationName(event.target.value)}
-          id="variation"
-        />
-        <br></br>
-        UPC Number
-        <input
-          value={UPCNumber}
-          label="UPC Number"
-          placeholder="UPC Number"
-          onChange={() => setUPCNumber(event.target.value)}
-          id="UPCNumber"
-        />
-        <br></br>
-        Vendor Style
-        <input
-          value={vendorStyle}
-          label="Vendor Style"
-          placeholder="Vendor Style"
-          onChange={() => setVendorStyle(event.target.value)}
-          id="vendorStyle"
-        />
-        <p>Categories</p>
-        {databaseCategories.categories &&
-          databaseCategories.categories.map((category) => {
-            return (
-              <div>
-                {category.name}
-                <input
-                  type="checkbox"
-                  name="categories"
-                  value={category.id}
-                  id={category.id}
+      <div className = 'wholesalerBar'>
+        <h1>New Card Info</h1>
+        <button className = 'pageButton' onClick = {() => history.push("/cards")}><ArrowBackIos/>Back</button> 
+      </div>
+      <div className = 'formContainer'>
+        <form>
+          Variation Name
+          <input
+            value={variationName}
+            label="Variation Name"
+            placeholder="Variation Name"
+            onChange={() => setVariationName(event.target.value)}
+            id="variation"
+          />
+          <br></br>
+          UPC Number
+          <input
+            value={UPCNumber}
+            label="UPC Number"
+            placeholder="UPC Number"
+            onChange={() => setUPCNumber(event.target.value)}
+            id="UPCNumber"
+          />
+          <br></br>
+          Vendor Style
+          <input
+            value={vendorStyle}
+            label="Vendor Style"
+            placeholder="Vendor Style"
+            onChange={() => setVendorStyle(event.target.value)}
+            id="vendorStyle"
+          />
+          <br/>
+          <br/>
+          <p>Select Categories:</p>
+          <div className = 'categoryInputs'>
+            {databaseCategories.categories &&
+              databaseCategories.categories.map((category) => {
+                return (
+                  <div>
+                    <input
+                      type="checkbox"
+                      name="categories"
+                      value={category.id}
+                      id={category.id}
+                      />
+                    {category.name}
+                  </div>
+                );
+              })}
+          </div>
+          <br/>
+          <h3>File Upload</h3>
+          <div className = 'fileInputs'>
+            <div className = 'filesLeft'>
+
+            </div>
+            <div className = 'filesRight'>
+
+            </div>
+            <div>
+              <label for="barcode">Barcode: </label>
+              <input
+                id="barcode"
+                type="file"
+                value={barcode}
+                onChange={() => {
+                  setBarcode(event.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label for="front">Front: </label>
+              <input
+                id="front"
+                type="file"
+                value={front}
+                onChange={() => {
+                  setFront(event.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label for="insideInsertion">Inside Insertion: </label>
+              <input
+                id="insideInsertion"
+                type="file"
+                value={insideInsertion}
+                onChange={() => {
+                  setInsideInsertion(event.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label for="insert">Insert: </label>
+              <input
+                id="insert"
+                type="file"
+                value={insert}
+                onChange={() => {
+                  setInsert(event.target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label for="sticker">sticker: </label>
+              <input
+                id="sticker"
+                type="file"
+                value={sticker}
+                onChange={() => {
+                  setSticker(event.target.value);
+                }}
                 />
-              </div>
-            );
-          })}
-        <label for="barcode">Barcode: </label>
-        <input
-          id="barcode"
-          type="file"
-          value={barcode}
-          onChange={() => {
-            setBarcode(event.target.value);
-          }}
-        />
-        <label for="front">Front: </label>
-        <input
-          id="front"
-          type="file"
-          value={front}
-          onChange={() => {
-            setFront(event.target.value);
-          }}
-        />
-        <label for="insideInsertion">Inside Insertion: </label>
-        <input
-          id="insideInsertion"
-          type="file"
-          value={insideInsertion}
-          onChange={() => {
-            setInsideInsertion(event.target.value);
-          }}
-        />
-        <label for="insert">Insert: </label>
-        <input
-          id="insert"
-          type="file"
-          value={insert}
-          onChange={() => {
-            setInsert(event.target.value);
-          }}
-        />
-        <label for="sticker">sticker: </label>
-        <input
-          id="sticker"
-          type="file"
-          value={sticker}
-          onChange={() => {
-            setSticker(event.target.value);
-          }}
-        />
-        <label for="tiffFile">TIFF File: </label>
-        <input
-          id="tiffFile"
-          type="file"
-          value={TIFFFile}
-          onChange={() => {
-            setTIFFFile(event.target.value);
-          }}
-        />
-        <label for="AIfile">AI File: </label>
-        <input
-          id="AIfile"
-          type="file"
-          value={AIFile}
-          onChange={() => {
-            setAIFile(event.target.value);
-          }}
-        />
-        Create Card
-        <button onClick={handleSubmit}>Submit</button>
-      </form>
+            </div>
+            <div>
+              <label for="tiffFile">TIFF File: </label>
+              <input
+                id="tiffFile"
+                type="file"
+                value={TIFFFile}
+                onChange={() => {
+                  setTIFFFile(event.target.value);
+                }}
+                />
+            </div>
+            <div>
+              <label for="AIfile">AI File: </label>
+              <input
+                id="AIfile"
+                type="file"
+                value={AIFile}
+                onChange={() => {
+                  setAIFile(event.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <button type='reset' onClick={handleClear}>Clear</button>
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
