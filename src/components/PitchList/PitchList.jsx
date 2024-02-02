65dc7fbb6afb756da8e8696d00a97f4abbcff3de
Mark Klein <kleincentral@gmail.com>
2024-02-02 14:06:32 -0600

    Code works, and filters correctly based off of filter choices

--- src/components/PitchList/PitchList.jsx
+++ src/components/PitchList/PitchList.jsx
@@ -11,20 +11,25 @@ import {
 } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { render } from "react-dom";
 
 export default function PitchList() {
   const pitches = useSelector((store) => store.pitches);
+  const wholesalers = useSelector(
+    (store) => store.wholesalersReducer.wholesalers
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: "SAGA/FETCH_PITCHES" });
+    dispatch({ type: "SAGA/FETCH_WHOLESALERS" });
 
     sortPitchByDateOldest();
   }, []);
 
   const [page, setPage] = useState(0);
-  const [wholesalerSort, setWholesalerSort] = useState(false);
   const [dateSort, setDateSort] = useState(false);
+  const [renderList, setRenderList] = useState(false);
 
   /**
    * This function allows you to sort pitches based off of oldest to newest.
@@ -43,6 +48,20 @@ export default function PitchList() {
       // dates must be equal
       return 0;
     });
+    if (renderList) {
+      renderList.sort((a, b) => {
+        const dateA = a.date;
+        const dateB = b.date;
+        if (dateA < dateB) {
+          return -1;
+        }
+        if (dateA > dateB) {
+          return 1;
+        }
+        // dates must be equal
+        return 0;
+      });
+    }
     setDateSort(false);
   };
 
@@ -63,11 +82,67 @@ export default function PitchList() {
       // dates must be equal
       return 0;
     });
+    if (renderList) {
+      renderList.sort((a, b) => {
+        const dateA = a.date;
+        const dateB = b.date;
+        if (dateA < dateB) {
+          return 1;
+        }
+        if (dateA > dateB) {
+          return -1;
+        }
+        // dates must be equal
+        return 0;
+      });
+    }
     setDateSort(true);
+  };
+
+  let result = false;
+
+  /**
+   * This function allows you to sort pitches based off of newest to oldest.
+   * @returns a sorted list based off of the date begun with, newest first.
+   */
+  const filterWholesalersByName = (name) => {
+    if (name != 0) {
+      result = pitches.filter((index) => index.wholesaler_id == name);
+    } else {
+      result = false;
+    }
+    findRenderList();
+  };
+
+  const findRenderList = () => {
+    if (result) {
+      setRenderList(result);
+    } else {
+      setRenderList(pitches);
+    }
   };
 
   return (
     <div className="container">
+      <p>
+        Wholesaler:{" "}
+        <span>
+          <select
+            onChange={() => filterWholesalersByName(event.target.value)}
+            id="framework"
+          >
+            <option value="0">All</option>
+            {wholesalers &&
+              wholesalers.map((wholesaler) => {
+                return (
+                  <option value={wholesaler.id}>
+                    {wholesaler.company_name}
+                  </option>
+                );
+              })}
+          </select>
+        </span>
+      </p>
       <Paper sx={{ width: "100%", overflow: "hidden" }}>
         <TableContainer>
           <Table stickyheader aria-label="sticky table">
@@ -79,12 +154,12 @@ export default function PitchList() {
                 <TableCell style={{ maxWidth: "10vw" }} key={"date"}>
                   {!dateSort && (
                     <span id={"filterV"} onClick={sortPitchByDateNewest}>
-                      Date V
+                      Date ▽
                     </span>
                   )}
                   {dateSort && (
                     <span id={"filterV"} onClick={sortPitchByDateOldest}>
-                      Date Λ
+                      Date △
                     </span>
                   )}
                 </TableCell>
@@ -95,8 +170,8 @@ export default function PitchList() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {pitches &&
-                pitches.map((pitchRow) => (
+              {(renderList &&
+                renderList.map((pitchRow) => (
                   <TableRow
                     hover
                     role="checkbox"
@@ -118,7 +193,33 @@ export default function PitchList() {
                       </Button>
                     </TableCell>
                   </TableRow>
-                ))}
+                ))) ||
+                (pitches &&
+                  pitches.map((pitchRow) => (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={pitchRow.id}
+                    >
+                      <TableCell>{pitchRow.wholesaler_company_name}</TableCell>
+                      <TableCell>
+                        {new Date(
+                          Date.parse(pitchRow.date)
+                        ).toLocaleDateString()}
+                      </TableCell>
+                      <TableCell>{pitchRow.description}</TableCell>
+                      <TableCell>
+                        <Button variant="outlined">View</Button>
+                        <span> </span>
+                        <Button variant="outlined">Edit</Button>
+                        <span> </span>
+                        <Button variant="contained" color="error">
+                          Delete
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  )))}
             </TableBody>
           </Table>
         </TableContainer>
