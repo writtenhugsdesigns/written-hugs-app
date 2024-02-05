8481b58e5a036956d980381a98602a2e2f3cec6b
James Woods <james.d.t.woods@gmail.com>
2024-02-05 15:51:23 -0600

    working on pitch review, need to style more

--- src/components/CreatePitchPage/CreatePitchPage.jsx
+++ src/components/CreatePitchPage/CreatePitchPage.jsx
@@ -10,7 +10,7 @@ import {
 } from '@mui/material';
 import './CreatePitchPage.css';
 
-export default function CreatePitchPage() {
+export default function CreatePitchPage({handleClose}) {
     const history = useHistory();
     const dispatch = useDispatch();
     const cardsByCategory = useSelector(store => store.cardsReducer.cardsListByCategory);
@@ -40,7 +40,7 @@ export default function CreatePitchPage() {
     return (
         <div className='container'>
             Create Pitch Page
-            <button onClick={toReview}>Create Pitch 🛒</button>
+            <button onClick={()=>toReview(handleClose)}>Create Pitch 🛒</button>
             <br /><br /><br />
             {cardsByCategory.map((category) => {
                 return (
--- src/components/ReviewPitch/ReviewPitch.jsx
+++ src/components/ReviewPitch/ReviewPitch.jsx
@@ -1,10 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import {
+    Button,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    TableContainer,
+    TablePagination,
+    Modal,
+    Box
+  } from "@mui/material";
 
 export default function ReviewPitch(){
-    const pitches = useSelector(store => store.pitches.pitches);
+    const newPitch = useSelector(store => store.pitches.newPitch);
     const history = useHistory();
+    const dispatch = useDispatch();
+
 
     // When user clicks the create pitch button, POST new pitch, then redirect user to view the pitch
     const handleCreate = () => {
@@ -14,13 +29,47 @@ export default function ReviewPitch(){
             payload: newPitch
         })
     }
+    const removeButton = (card) => {
+        dispatch({
+            type: 'REMOVE_CARD_FROM_PITCH',
+            payload: card
+        })
+    }
     
-console.log('pitches', pitches);
     return (
         <div className = 'container'>
             Review Pitch
-            <button onClick = {() => history.push("/createPitch")}>Back</button>
+            <button onClick = {() => history.push("/")}>Back</button>
             <button onClick = {handleCreate}>Create</button>
-        </div>
+              {/* MUI table within an MUI paper component */}
+              <Paper sx={{ width: "100%", overflow: "hidden" }}>
+                <TableContainer>
+                  <Table stickyheader aria-label="sticky table">
+                    <TableHead>
+                      <TableRow sx={{backgroundColor: '#eeebe5'}}>
+                        <TableCell>Card Name</TableCell>
+                        <TableCell>Categories</TableCell>
+                        <TableCell></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {newPitch &&
+                        newPitch.map((card) => (
+                          <TableRow hover role="checkbox" tabIndex={-1} key={card.id}>
+                            <TableCell>{card.name}</TableCell>
+                            <TableCell>{card.categories_array.map((category) => (<span className='tag'>{category.category_name}</span>))}</TableCell>
+                            <TableCell>
+                              <Button onClick = {() => removeButton(card)} variant="contained" color="error">
+                                Remove from pitch
+                              </Button>
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Paper>
+        
+            </div>
     )
 }
