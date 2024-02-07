7590f6be4447725cc8eba07b6aa937e6ad305ef6
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-07 15:23:25 -0600

    Fixed page refresh on categories page, added visual indicator to cards that are in the cart, and made review pitch work with cart

--- src/components/ReviewPitch/ReviewPitch.jsx
+++ src/components/ReviewPitch/ReviewPitch.jsx
@@ -19,7 +19,7 @@ export default function ReviewPitch() {
     let [description, setDescription] = useState('');
     let [name, setName] = useState('');
     let [wholesaler_id, setWholesaler_id] = useState('');
-    const newPitch = useSelector(store => store.pitches.newPitch);
+    const cart = useSelector(store => store.cartReducer.cart);
     const wholesalers = useSelector(store => store.wholesalersReducer.wholesalers);
     const history = useHistory();
     const dispatch = useDispatch();
@@ -38,14 +38,14 @@ export default function ReviewPitch() {
                 pitchName: name,
                 pitchDescription: description,
                 wholesaler_id: wholesaler_id,
-                newPitch: newPitch
+                newPitch: cart
             }
         })
         history.push("/pitches");
     }
     const removeButton = (card) => {
         dispatch({
-            type: 'REMOVE_CARD_FROM_PITCH',
+            type: 'REMOVE_CARD_FROM_CART',
             payload: card
         })
     }
@@ -91,14 +91,14 @@ export default function ReviewPitch() {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {newPitch &&
-                                newPitch.map((card) => (
+                            {cart &&
+                                cart.map((card) => (
                                     <TableRow hover role="checkbox" tabIndex={-1} key={card.id}>
                                         <TableCell>{card.name}</TableCell>
                                         <TableCell>{card.categories_array.map((category) => (<span className='tag'>{category.category_name}</span>))}</TableCell>
                                         <TableCell>
                                             <Button onClick={() => removeButton(card)} variant="contained" color="error">
-                                                Remove from pitch
+                                                Remove
                                             </Button>
                                         </TableCell>
                                     </TableRow>
@@ -107,7 +107,6 @@ export default function ReviewPitch() {
                     </Table>
                 </TableContainer>
             </Paper>
-
         </div>
     )
 }
