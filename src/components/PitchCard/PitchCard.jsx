499e9ac3c8845328d2bb51efdb1e3c6394abae83
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-07 11:52:32 -0600

    Gave functionality to the add all button on the pitch category page and made a new reducer clause

--- src/components/PitchCard/PitchCard.jsx
+++ src/components/PitchCard/PitchCard.jsx
@@ -1,76 +1,95 @@
-import React, { useState,  useEffect } from "react"
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+import { useDispatch } from "react-redux";
 
-export default function PitchCard({card}){
+export default function PitchCard({ card, isInCart }) {
+  const [isHoveredID, setIsHoveredID] = useState("");
+  const dispatch = useDispatch();
 
-    const [isHoveredID, setIsHoveredID] = useState('');
-    const dispatch = useDispatch();
-    const cart = useSelector(store => store.cartReducer.cart);
+  const removeCardFromCart = () => {
+    dispatch({
+      type: "REMOVE_CARD_FROM_CART",
+      payload: card,
+    });
+  };
 
-    useEffect(() => {
-        dispatch({type: 'GET_CART'});
-    }, []);
+  const addCardToCart = () => {
+    dispatch({
+      type: "ADD_CARD_TO_CART",
+      payload: card,
+    });
+  };
 
-    const removeCardFromCart = () => { 
-        dispatch({
-            type: 'REMOVE_FROM_CART',
-            payload: card
-        })
-    }
-
-    const addCardToCart = () => {
-        dispatch({
-            type: 'ADD_TO_CART',
-            payload: card
-        })
-    }
-
-    /**
-     * 
-     * @returns a boolean value indicating whether a specific card has been added
-     * to the pitch cart
-     */
-    const isInCart = () => {
-        for(let object of cart){
-            if(object.card_id == card.card_id){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    return (
-        <Card sx={{height: '25em', width: '15em', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-            <CardMedia
-                height='20em'
-                onMouseOver={() => setIsHoveredID(card.card_id)}
-                onMouseOut={() => setIsHoveredID('')}
-            >
-                {isHoveredID == '' ?
-                    <img height={'200'} src={`${card.front_img.display}`}/>
-                    :
-                    <img height={'200'} src={`${card.inner_img.display}`}/>
-                }
-                
-            </CardMedia>
-            <CardContent>
-                <Typography sx={{textAlign: 'center'}} gutterBottom variant='h5' component='div'>
-                    {card.name}
-                </Typography>
-                <Typography sx={{overflowY: 'auto', height: '4em'}}variant='body2' color='text.secondary'>
-                    {card.description}
-                </Typography>
-            </CardContent>
-            <CardActions sx={{marginTop: 'auto', marginBottom: '3px', justifyContent: 'center'}}>
-                <Button variant='outlined' size='medium'>View</Button>
-                {!isInCart() && 
-                    <Button variant='contained' size='medium' onClick={addCardToCart}>Add</Button>
-                }
-                {isInCart() &&
-                    <Button variant='contained' color='error' onClick={removeCardFromCart}>Remove</Button>
-                }
-            </CardActions>
-        </Card>
-    )
+  return (
+    <Card
+      sx={{
+        height: "25em",
+        width: "15em",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <CardMedia
+        height="20em"
+        onMouseOver={() => setIsHoveredID(card.card_id)}
+        onMouseOut={() => setIsHoveredID("")}
+      >
+        {isHoveredID == "" ? (
+          <img height={"200"} src={`${card.front_img.display}`} />
+        ) : (
+          <img height={"200"} src={`${card.inner_img.display}`} />
+        )}
+      </CardMedia>
+      <CardContent>
+        <Typography
+          sx={{ textAlign: "center" }}
+          gutterBottom
+          variant="h5"
+          component="div"
+        >
+          {card.name}
+        </Typography>
+        <Typography
+          sx={{ overflowY: "auto", height: "4em" }}
+          variant="body2"
+          color="text.secondary"
+        >
+          {card.description}
+        </Typography>
+      </CardContent>
+      <CardActions
+        sx={{
+          marginTop: "auto",
+          marginBottom: "3px",
+          justifyContent: "center",
+        }}
+      >
+        <Button variant="outlined" size="medium">
+          View
+        </Button>
+        {!isInCart(card) && (
+          <Button variant="contained" size="medium" onClick={addCardToCart}>
+            Add
+          </Button>
+        )}
+        {isInCart(card) && (
+          <Button
+            variant="contained"
+            color="error"
+            onClick={removeCardFromCart}
+          >
+            Remove
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+  );
 }
--- src/redux/reducers/cart.reducer.js
+++ src/redux/reducers/cart.reducer.js
@@ -1,12 +1,32 @@
 import { combineReducers } from 'redux';
 
+/**
+ * @returns a boolean value indicating whether a specific card has been added
+ *  to the pitch cart
+ */
+const isInCart = (card, cart) => {
+    for(let object of cart){
+        if(object.card_id == card.card_id){
+            return true;
+        }
+    }
+    return false;
+}
+
 const cart = (state = [], action) => {
-    if(action.type === 'ADD_TO_CART'){
+    if(action.type === 'ADD_CARD_TO_CART'){
         return [...state, action.payload]
-    } else if(action.type === 'REMOVE_FROM_CART'){
+    } else if(action.type === 'REMOVE_CARD_FROM_CART'){
         return state.filter((card) => {
             return card.card_id !== action.payload.card_id;
         })
+    } else if(action.type === 'ADD_CATEGORY_TO_CART'){
+        const cards = action.payload.filter((card) => {
+            return !isInCart(card, state);
+        });
+        return [...state, ...cards];
+    } else if(action.type === 'REMOVE_CATEGORY_FROM_CART'){
+
     } else if(action.type === 'GET_CART'){
         return state;
     } else if(action.type === 'CLEAR_CART'){
