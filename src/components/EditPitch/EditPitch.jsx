c6e7dd1edf03ecb2734feb2b29c0f07712b73a91
Mark Klein <kleincentral@gmail.com>
2024-02-07 14:34:37 -0600

    Need main stuff so saving

--- src/components/EditPitch/EditPitch.jsx
+++ src/components/EditPitch/EditPitch.jsx
@@ -1,27 +1,36 @@
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import PitchCategory from "../PitchCategory/PitchCategory";
 
 export default function EditPitch() {
   const dispatch = useDispatch();
+  const history = useHistory();
   const { id } = useParams();
   const pitches = useSelector((store) => store.pitches.pitches);
+  const cardsByCategory = useSelector(store => store.cardsReducer.cardsListByCategory);
+
 
   useEffect(() => {
     dispatch({ type: "SAGA/FETCH_PITCHES" });
+    dispatch({ type: "SAGA/FETCH_CARDS_BY_CATEGORY" });
   }, []);
 
   const selectedPitch = pitches.filter(
     (individualPitch) => individualPitch.pitches_id == id
   );
 
+  console.log(selectedPitch)
+
   return (
-    <div>
-      <p>
-        hi to the pitch description of{" "}
-        {selectedPitch[0] && selectedPitch[0].description}
-      </p>
+    <div className='container'>
+      <div className='wholesalerBar'>
+        <h1>Edit Pitch</h1>
+        <button className='pageButton' onClick={() => history.push("/reviewPitch")}> 🛒 Review Pitch (#)</button>
+      </div>
+      {cardsByCategory.map((x, i) => {
+                return <PitchCategory key={i} categoryContents={x}/>
+            })}
     </div>
   );
 }
