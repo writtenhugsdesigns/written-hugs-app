5ae1071fdb38cca7c6acb60ea43992c1b1d2652b
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-08 16:00:19 -0600

    More styling

--- src/components/PitchCard/PitchCard.jsx
+++ src/components/PitchCard/PitchCard.jsx
@@ -60,13 +60,14 @@ export default function PitchCard({ card, isInCart }) {
     <>
       <Card
         sx={{
-        height: "25em",
+        height: "27em",
         width: "15em",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
       }}
       >
+        {isInCart(card) && <Alert>Added</Alert>}
         <CardMedia
           height="20em"
           onMouseOver={() => setIsHoveredID(card.card_id)}
@@ -84,6 +85,7 @@ export default function PitchCard({ card, isInCart }) {
             gutterBottom
             variant="h5"
             component="div"
+            fontFamily= 'Open Sans Light'
           >
             {card.name}
           </Typography>
@@ -91,6 +93,7 @@ export default function PitchCard({ card, isInCart }) {
             sx={{ overflowY: "auto", height: "4em" }}
             variant="body2"
             color="text.secondary"
+            fontFamily='Open Sans Light'
           >
             {card.description}
           </Typography>
