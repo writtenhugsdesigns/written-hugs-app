7a7948b8cb96740545ba19e0f6b4214b432705b8
Hannah Bjorklund <bjorklundhannah@gmail.com>
2024-02-09 15:17:26 -0600

    cleanup and styling for categories page

--- src/components/CategoriesPage/CategoriesPage.jsx
+++ src/components/CategoriesPage/CategoriesPage.jsx
@@ -1,34 +1,37 @@
 import { useState } from "react";
-import {Modal, Box} from '@mui/material';
+import { Modal, Box } from "@mui/material";
 import { AddCircleOutline } from "@mui/icons-material";
 import { smallModalStyle } from "../../constants/styling";
-import CreateCategory from '../CreateCategory/CreateCategory'
+import CreateCategory from "../CreateCategory/CreateCategory";
 import CategoriesList from "../CategoriesList/CategoriesList";
 
 export default function CategoriesPage() {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    
-    return (
-        <>
-            <div className='container'>
-                <div className='wholesalerBar'>
-                    <h1>Categories</h1>
-                    <button className='pageButton' onClick={handleOpen}><AddCircleOutline/>Add Category</button>
-                </div>
-                <CategoriesList />
-            </div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={smallModalStyle}>
-                    <CreateCategory handleClose={handleClose} />
-                </Box>
-            </Modal>
-        </>
-    )
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <div className="container">
+        <div className="wholesalerBar">
+          <h1>Categories</h1>
+          <button className="pageButton" onClick={handleOpen}>
+            <AddCircleOutline />
+            Add Category
+          </button>
+        </div>
+        <CategoriesList />
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={smallModalStyle}>
+          <CreateCategory handleClose={handleClose} />
+        </Box>
+      </Modal>
+    </>
+  );
 }
